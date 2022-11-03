8661ecd69c3fcda975fa47ad2ab1879f81daa9d2
niicode <niidarku66@gmail.com>
2022-11-03 08:46:10 +0000

    Fix linter errors

--- Person.rb
+++ Person.rb
@@ -1,21 +1,23 @@
+ 
+
 class Person
-    attr_accessor :name, :age
-    attr_reader :id
-  
-    def initialize(age, name: 'unknown', parent_permission: true)
-      @id = Random.rand(1..100)
-      @age = age
-      @name = name
-      @parent_permission = parent_permission
-    end
-  
-    def can_use_services?
-      of_age? || @parent_permission
-    end
-  
-    private
-  
-    def of_age?
-      age >= 18
-    end
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name: 'unknown', parent_permission: true)
+    @id = Random.rand(1..100)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
   end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    age >= 18
+  end
+end
