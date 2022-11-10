18bc390b8550231ccead0aa372198139e90bf265
Inieke Sendy <ini.e.sendy@gmail.com>
2022-11-10 14:41:10 +0100

    [unit-test]: add test for students

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,11 +1,11 @@
-# require_relative './../student_file'
+require_relative './../student_file'
 
-# describe Student do
-#   context 'When testing the Student class' do
-#     it 'should create a new Student when student class is instantiated' do
-#       student = Student.new(23,'primary 3', 'Inieke')
-#       expect(student.age).to eq 23
-#       expect(student.name).to eq 'Inieke'
-#     end
-#   end
-# end
+describe Student do
+  context 'When testing the Student class' do
+    it 'should create a new Student when student class is instantiated' do
+      student = Student.new(23,'primary 3', 'Inieke')
+      expect(student.age).to eq 23
+      expect(student.name).to eq 'Inieke'
+    end
+  end
+end
