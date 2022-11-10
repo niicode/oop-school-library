ad2d7b88c9436b52008b336685966f2213ece2bb
Sendy Inieke <95294594+uisendy@users.noreply.github.com>
2022-11-10 17:07:33 +0100

    [unit-test]: fix Linters

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -3,7 +3,7 @@ require_relative './../teacher_file'
 describe Teacher do
   context 'When testing the Teacher class' do
     it 'should create a new teacher when teacher class is instantiated' do
-      teacher = Teacher.new(34,'Ruby on Rails', 'Duane David')
+      teacher = Teacher.new(34, 'Ruby on Rails', 'Duane David')
       expect(teacher.specialization).to eq 'Ruby on Rails'
       expect(teacher.name).to eq 'Duane David'
       expect(teacher.age).to eq 34
