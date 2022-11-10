183c90418a5f282d273c55cfd083ea8194b48175
Inieke Sendy <ini.e.sendy@gmail.com>
2022-11-10 14:41:01 +0100

    [unit-test]: add test for rentals

--- spec/rentals_spec.rb
+++ spec/rentals_spec.rb
@@ -1,15 +1,15 @@
-# require_relative './../rental'
-# require_relative './../teacher_file'
-# require_relative './../book'
+require_relative './../rental'
+require_relative './../teacher_file'
+require_relative './../book'
 
-# describe Book do
-#   context 'When testing the Rental class' do
-#     it 'should create a new Rental when rental class is instantiated' do
-#       teacher = Teacher.new('Ruby', 34, 'William Raga')
-#       book = Book.new('title', 'author')
-#       rental = Rental.new('2022-09-09', teacher, book)
-#       expect(rental.book.rentals.length).to eq 1
-#       expect(rental.person.rentals.length).to eq 1
-#     end
-#   end
-# end
+describe Book do
+  context 'When testing the Rental class' do
+    it 'should create a new Rental when rental class is instantiated' do
+      teacher = Teacher.new('Ruby', 34, 'William Raga')
+      book = Book.new('title', 'author')
+      rental = Rental.new('2022-09-09', teacher, book)
+      expect(rental.book.rentals.length).to eq 1
+      expect(rental.person.rentals.length).to eq 1
+    end
+  end
+end
